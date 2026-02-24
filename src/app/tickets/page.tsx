d2e4b9a4328ba1a86329a1eaacd034db557968d6
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-02-25 06:15:52 +0800

    feat: add url

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,9 +3,12 @@ import Heading from "@/components/heading";
 import Spinner from "@/components/spinner";
 import TicketList from "@/features/tickets/components/ticket-list";
 import TicketUpsertForm from "@/features/tickets/components/ticket-upsert-form";
+import { getBaseUrl } from "@/utils/url";
 import { Suspense } from "react";
 
 const TicketsPage = () => {
+  console.log(getBaseUrl());
+
   return (
     <div className="flex flex-col gap-y-8 flex-1">
       <Heading title="Tickets" description="All your tickets at one place" />
