f5ba475a14e391dcfc98fc2dfbdbb697a868e622
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-02-25 05:49:25 +0800

    feat: handle unhappy path for delete ticket

--- src/features/tickets/actions/delete-ticket.ts
+++ src/features/tickets/actions/delete-ticket.ts
@@ -1,19 +1,22 @@
 "use server";
 
 import { setCookieByKey } from "@/actions/cookies";
+import fromErrorToActionState from "@/components/form/utils/to-action-state";
 import prisma from "@/lib/prisma";
 import { ticketsPagePath } from "@/path";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 const deleteTicket = async (id: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
-  await prisma.ticket.delete({
-    where: {
-      id,
-    },
-  });
+  try {
+    await prisma.ticket.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return fromErrorToActionState(error);
+  }
 
   revalidatePath(ticketsPagePath());
 
