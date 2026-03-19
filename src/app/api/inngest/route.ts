9a8013aab2c2f19ad9b54930bfc28f8538b06082
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-03-19 08:35:46 +0800

    call email verification in route.ts

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,8 +3,14 @@ import { inngest } from "@/lib/inngest";
 import { passwordResetEvent } from "@/features/password/inngest/event-password-reset";
 import { passwordResetEventSuccess } from "@/features/password/inngest/event-password-reset-success";
 import { emailChangeEvent } from "@/features/password/inngest/event-email-change";
+import { eventEmailVerification } from "@/features/password/inngest/event-email-verification";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [passwordResetEvent, passwordResetEventSuccess, emailChangeEvent],
+  functions: [
+    passwordResetEvent,
+    passwordResetEventSuccess,
+    emailChangeEvent,
+    eventEmailVerification,
+  ],
 });
