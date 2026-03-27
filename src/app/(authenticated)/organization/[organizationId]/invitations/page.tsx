ca66da436a246792ebbde3552dd5029b5620bca4
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-03-27 12:24:26 +0800

    run type error in invitation page

--- src/app/(authenticated)/organization/[organizationId]/invitations/page.tsx
+++ src/app/(authenticated)/organization/[organizationId]/invitations/page.tsx
@@ -2,6 +2,9 @@ import Heading from "@/components/heading";
 import OrganizationBreadcrumbs from "@/components/organization-breadcrumbs";
 import Spinner from "@/components/spinner";
 import InvitationList from "@/features/invitations/components/invitation-list";
+import { auth } from "@/lib/auth";
+import { organization } from "better-auth/plugins";
+import { headers } from "next/headers";
 import { Suspense } from "react";
 
 type InvitationPageProps = {
@@ -10,12 +13,24 @@ type InvitationPageProps = {
 
 const InvitationPage = async ({ params }: InvitationPageProps) => {
   const { organizationId } = await params;
+
+  const organization = await auth.api.getFullOrganization({
+    headers: await headers(),
+    query: {
+      organizationId,
+    },
+  });
+
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading
         title="Invitations"
         description="Manage your organization invitations."
-        tabs={<OrganizationBreadcrumbs />}
+        tabs={
+          <OrganizationBreadcrumbs
+            organizationName={organization?.name ?? "Organization"}
+          />
+        }
       />
 
       <Suspense fallback={<Spinner />}>
