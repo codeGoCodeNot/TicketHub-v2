3f24de511c3b385bc4329534674324a14305c9d8
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-03-29 11:14:29 +0800

    setting up aws s3

---
+++ environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production" | "test";
+      AWS_ACCESS_KEY_ID: string;
+      AWS_SECRET_ACCESS_KEY: string;
+      AWS_REGION: string;
+      AWS_BUCKET_NAME: string;
+    }
+  }
+}
+
+export default global;
