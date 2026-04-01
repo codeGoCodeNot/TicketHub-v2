b1825327f14821122adeb79ba818d38981114506
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-04-01 19:52:22 +0800

    refactor: extract comment and member data access layer

--- src/features/membership/data/update-member.ts
+++ src/features/membership/data/update-member.ts
@@ -1,1 +1,14 @@
-export const updateMember = async () => {};
+import { Prisma } from "@/generated/prisma/client";
+import prisma from "@/lib/prisma";
+
+export const updateMember = async (
+  id: string,
+  data: Prisma.MemberUpdateInput,
+) => {
+  return await prisma.member.update({
+    where: {
+      id,
+    },
+    data,
+  });
+};
