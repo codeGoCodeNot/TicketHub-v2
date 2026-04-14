3051b2cea2eed44219d51e470d74842c91e8c044
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-04-15 02:41:46 +0800

    feat: add auth header with user refetch on route change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,12 +8,26 @@ import AccountDropdown from "./account-dropdown";
 import ThemeSwitcher from "./theme/theme-switcher";
 import { Button } from "./ui/button";
 import { SidebarTrigger } from "./ui/sidebar";
+import { useEffect, useState } from "react";
+import { User } from "better-auth/types";
+import { usePathname } from "next/navigation";
+import { getAuthUser } from "@/features/auth/actions/get-auth-user";
 
 const Header = () => {
-  const { data: session, isPending } = authClient.useSession();
-  const user = session?.user;
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const pathName = usePathname();
 
-  if (isPending) return null;
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getAuthUser();
+      setUser(user);
+      setLoading(false);
+    };
+    fetchUser();
+  }, [pathName]);
+
+  if (loading) return null;
 
   const navItems = user ? (
     <>
@@ -30,8 +44,10 @@ const Header = () => {
     </>
   );
 
+  if (loading) return null;
+
   return (
-    <nav className="w-full flex py-2.5 px-5 justify-between border-b bg-background/95 backdrop-blur supports-backdrop-blur:bg-background/60 fixed top-0 right-0 left-0 z-20 animate-slide-from-top">
+    <nav className="w-full flex py-2.5 px-5 justify-between border-b bg-background/95 backdrop-blur supports-backdrop-blur:bg-background/60 fixed top-0 right-0 left-0 z-20 animate-fade-from-top">
       <div className="flex gap-x-2 items-center">
         <Button asChild variant="ghost" className="font-semibold text-lg">
           <Link href={homePagePath()}>
@@ -40,7 +56,6 @@ const Header = () => {
           </Link>
         </Button>
       </div>
-
       <div className="flex gap-x-2 items-center">
         <ThemeSwitcher />
         {navItems}
